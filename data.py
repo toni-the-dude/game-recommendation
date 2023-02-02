ce7e6cf79d422dbab8998ea89edef3ea6b87a0af
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-02-03 00:34:02 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,4 @@
+data = {
+    # [id - only if tag node, title, tags, desc]
+    [None, "Risk of Rain 2", ["roguelike", "multiplayer", "third-person shooter"], "Escape a chaotic alien planet by fighting through hordes of frenzied monsters – with your friends, or on your own. Combine loot in surprising ways and master each character until you become the havoc you feared upon your first crash landing."]
+       }
