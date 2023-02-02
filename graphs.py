ec06a4de8175b28ffb464d341fa0eb9b5fdacf3e
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-02-03 00:08:33 +0200

    Starting with graphs.

--- graphs.py
+++ graphs.py
@@ -1,1 +1,12 @@
+class Vertex:
+  """Key methods of Vertex class"""
+  def __init__(self, value): pass
+  def add_edge(self, vertex, weight = 0): pass
+  def get_edges(self): pass
 
+class Graph:
+  """Key methods of Graph class"""
+  def __init__(self, directed = False): pass
+  def add_vertex(self, vertex): pass
+  def add_edge(self, from_vertex, to_vertex, weight = 0): pass
+  def find_path(self, start_vertex, end_vertex): pass
