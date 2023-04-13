fe93c867c61d752f490e0d6061a80342c9d423ba
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-04-13 19:40:01 +0300

    Make edge adding easier

--- graphs.py
+++ graphs.py
@@ -1,5 +1,5 @@
-class Node:
-  # Might be sufficient to build the entire program
+class Node: # Might be sufficient to build the entire program
+
   def __init__(self, name="Missing name", tags=[], description="No description", price="NA"): 
       self.name = name
       self.tags = tags
@@ -10,10 +10,18 @@ class Node:
         print("Created Node object '{}'. It represents a category.".format(self.name))
       else:
         print("Created Node object '{}'. It represents a game.".format(self.name))
+
   def __str__(self):
     return self.name
-  def add_edge(self, vertex): 
-      self.edges.append(vertex)
+
+  def add_edges(self, tagObjects): # Looks amongst tag objects and appends them accordingly
+      for tag in self.tags:
+        if tag in tagObjects:
+            print("Correctly matched tag.")
+            self.edges.append(tagObject[index(tag)])
+        else:
+            print("There is no such tag.")
+
   def get_edges(self): 
       print("Node {} has a total of {} edges. They lead to:".format(self.name, len(self.edges)))
       for edge in self.edges:
@@ -25,6 +33,12 @@ class Graph:
     self.games = []
     self.tags = []
     print("Created Graph object.")
+
+  def __contains__(self, node_name):
+    if node_name[0] == '#':
+        return any(node_name == tag.name for tag in self.tags)
+    return any(node_name == game.name for game in self.games)
+
   def add_vertex(self, vertex): 
     if vertex.name[0] == '#':
         self.tags.append(vertex)
@@ -32,5 +46,7 @@ class Graph:
         self.games.append(vertex)
     print("The games-side of the graph now looks as such:", [node.name for node in self.games])
     print("The category-side of the graph now looks as such:", [node.name for node in self.tags])
+
 #   def add_edge(self, from_vertex, to_vertex, weight = 0): pass
+
 #   def find_path(self, start_vertex, end_vertex): pass
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from graphs import *
 from data import *
-
+# Create tags
 tag1 = Node(tagData[0])
 tag2 = Node(tagData[1])
 tag3 = Node(tagData[2])
+# Test tags
 print(tag1)
 print(tag2)
 print(tag3)
-
+# Create games
 game1 = Node(gameData[0][0], gameData[0][1], gameData[0][2], gameData[0][3])
+# Test games
 print(game1.name)
 print(game1.tags)
 print(game1.description)
 print(game1.price)
-game1.add_edge(tag1)
-game1.add_edge(tag2)
-game1.add_edge(tag3)
-game1.get_edges()
-
+# Create graph
 graph = Graph()
 graph.add_vertex(game1)
 graph.add_vertex(tag1)
 graph.add_vertex(tag2)
 graph.add_vertex(tag3)
+# Add edges
+game1.add_edges(graph.tags)
+game1.get_edges()
