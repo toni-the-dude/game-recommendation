1c395ed5c9a7f74eb11717f6612ed1d802da8923
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-04-13 11:21:49 +0300

    Combine Tag & Game into Node class

--- graphs.py
+++ graphs.py
@@ -1,8 +1,8 @@
-class Game:
+class Node:
   # Unit of graph, contains information on a game  
   def __init__(self, name="Missing name", tags=[], description="No description", price="0$"): 
       self.name = name # May override how this gets printed
-      self.tags = tags
+      self.tags = tags # These could be the edges
       self.description = description # May override how this gets printed
       self.price = price
       self.edges = []
@@ -12,19 +12,10 @@ class Game:
       for edge in self.edges:
           print(edge)
 
-class Tag:
-  # Unit of graph, contains information on a category of game
-  def __init__(self, name=""): 
-      self.name = name
-      self.edges = []
-  def add_edge(self, vertex): 
-      Game.add_edge(self, vertex)
-  def get_edges(self): 
-      Game.get_edges(self)
-
 class Graph:
   # Here is where the games and tags will connect
-  def __init__(self): pass
+  def __init__(self, node): 
+    self.node = node
   def add_vertex(self, vertex): pass
   def add_edge(self, from_vertex, to_vertex, weight = 0): pass
   def find_path(self, start_vertex, end_vertex): pass
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from graphs import *
 from data import *
 
-game1 = Game(gameData[0][0], gameData[0][1], gameData[0][2], gameData[0][3])
+game1 = Node(gameData[0][0], gameData[0][1], gameData[0][2], gameData[0][3])
 print(game1.name)
 print(game1.tags)
 print(game1.description)
 print(game1.price)
 
-tag1 = Tag(tagData[0])
-tag2 = Tag(tagData[1])
-tag3 = Tag(tagData[2])
+tag1 = Node(tagData[0])
+tag2 = Node(tagData[1])
+tag3 = Node(tagData[2])
 print(tag1.name)
 print(tag2.name)
 print(tag3.name)
