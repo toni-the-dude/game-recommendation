c677af03e90a558da454bff7605ba2b0603590a1
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-04-13 11:49:43 +0300

    Test graph object

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 gameData = [
     # [name, tags, desc, price]
-    ["Risk of Rain 2", ["roguelike", "multiplayer", "third-person shooter"], "Escape a chaotic alien planet by fighting through hordes of frenzied monsters with your friends, or on your own. Combine loot in surprising ways and master each character until you become the havoc you feared upon your first crash landing.", "24.99$"]
+    ["Risk of Rain 2", ["#roguelike", "#multiplayer", "#third-person shooter"], "Escape a chaotic alien planet by fighting through hordes of frenzied monsters with your friends, or on your own. Combine loot in surprising ways and master each character until you become the havoc you feared upon your first crash landing.", "24.99$"]
 ]
 tagData = [
     # [name]
-    "roguelike", "multiplayer", "third-person shooter"
+    "#roguelike", "#multiplayer", "#third-person shooter"
 ]
--- graphs.py
+++ graphs.py
@@ -1,11 +1,17 @@
 class Node:
   # Unit of graph, contains information on a game  
-  def __init__(self, name="Missing name", tags=[], description="No description", price="0$"): 
+  def __init__(self, name="Missing name", tags=[], description="No description", price="NA"): 
       self.name = name # May override how this gets printed
       self.tags = tags # These could be the edges
       self.description = description # May override how this gets printed
       self.price = price
       self.edges = []
+      if self.name[0] == '#':
+        print("Created Node object '{}'. It represents a category.".format(self.name))
+      else:
+        print("Created Node object '{}'. It represents a game.".format(self.name))
+  def __str__(self):
+    return self.name
   def add_edge(self, vertex): 
       self.edges.append(vertex)
   def get_edges(self): 
@@ -14,8 +20,13 @@ class Node:
 
 class Graph:
   # Here is where the games and tags will connect
-  def __init__(self, node): 
-    self.node = node
-  def add_vertex(self, vertex): pass
+  def __init__(self): 
+    self.nodes = []
+    print("Created Graph object.")
+#   def __str__(self):
+    # return self.nodes
+  def add_vertex(self, vertex): 
+    self.nodes.append(vertex)
+    print("The graph now looks as such:", [node.name for node in self.nodes])
   def add_edge(self, from_vertex, to_vertex, weight = 0): pass
   def find_path(self, start_vertex, end_vertex): pass
