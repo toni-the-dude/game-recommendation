d568c088d46aceffefb43f5409f4c1f4b5255e4b
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-04-14 15:22:10 +0300

    Add data

--- data.py
+++ data.py
@@ -1,9 +1,18 @@
 tagData = [
     # [name]
-    "#roguelike", "#multiplayer", "#third-person shooter"
+    "#roguelike", "#co-op", "#looter-shooter", "exploration", "#deckbuilder", "#turn-based", "#strategy", "#historical", "#metroidvania", "#platformer", "#survival", "#open-world", "#pirates", "#city-builder", "#god-game", "#hack-and-slash", "#rpg", "#dungeon-crawler"
 ]
 
 gameData = [
     # [name, tags, desc, price]
-    ["Risk of Rain 2", ["#roguelike", "#multiplayer", "#third-person shooter"], "Escape a chaotic alien planet by fighting through hordes of frenzied monsters with your friends, or on your own. Combine loot in surprising ways and master each character until you become the havoc you feared upon your first crash landing.", "24.99$"]
+    ["Risk of Rain 2", ["#roguelike", "#co-op", "#looter-shooter"], "Escape a chaotic alien planet by fighting through hordes of frenzied monsters with your friends, or on your own. Combine loot in surprising ways and master each character until you become the havoc you feared upon your first crash landing.", "24.99€"],
+    ["Deep Rock Galactic", ["#co-op", "#looter-shooter", "#exploration"], "Deep Rock Galactic is a 1-4 player co-op FPS featuring badass space Dwarves, 100% destructible environments, procedurally-generated caves, and endless hordes of alien monsters.", "29,99€"],
+    ["Slay the Spire", ["#roguelike", "#deckbuilder", "#turn-based"], "We fused card games and roguelikes together to make the best single player deckbuilder we could. Craft a unique deck, encounter bizarre creatures, discover relics of immense power, and Slay the Spire!", "20,99€"],
+    ["Sid Meier’s Civilization VI", ["#strategy", "#turn-based", "#historical"], "Civilization VI is the newest installment in the award winning Civilization Franchise. Expand your empire, advance your culture and go head-to-head against history’s greatest leaders. Will your civilization stand the test of time?", "59,99€"],
+    ["Hollow Knight", ["#metroidvania", "#platformer", "#exploration"], "Forge your own path in Hollow Knight! An epic action adventure through a vast ruined kingdom of insects and heroes. Explore twisting caverns, battle tainted creatures and befriend bizarre bugs, all in a classic, hand-drawn 2D style.", "14,79€"],
+    ["Terraria", ["#survival", "#open-world", "#co-op"], "Dig, fight, explore, build! Nothing is impossible in this action-packed adventure game. Four Pack also available!", "9,99€"],
+    ["Sea of Thieves", ["#open-world", "#co-op", "#pirates"], "Sea of Thieves offers the essential pirate experience, from sailing and fighting to exploring and looting – everything you need to live the pirate life and become a legend in your own right. With no set roles, you have complete freedom to approach the world, and other players, however you choose.", "39,99€"],
+    ["Kainga", ["#city-builder", "roguelike", "#god-game"], "Welcome to the mystical world of Kainga, a unique roguelite village-builder. Take control of a Thinker and, with the help of your people, grow a unique town to complete short challenges while discovering the world and adapting to the pressures of the environment, beasts and other tribes.", "16,99€"],
+    ["Hades", ["#roguelike", "#hack-and-slash", "#rpg"], "Defy the god of the dead as you hack and slash out of the Underworld in this rogue-like dungeon crawler from the creators of Bastion, Transistor, and Pyre.", "24,50€"],
+    ["Barony", ["#roguelike", "#rpg", "#dungeon-crawler"], "Barony is the premier first-person roguelike with cooperative play. Cryptic items, brutal traps, and devious monsters await you. Conquer the dungeon alone, or gather a party in co-op with iconic and unusual RPG classes. Get ready to try, and die, and try again with a new dungeon every time you play!", "12,49€"]
 ]
