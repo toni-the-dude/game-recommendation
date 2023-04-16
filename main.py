7daa8db38ed08e6223575491909ce027c98c4919
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-04-16 17:10:47 +0300

    Add data with loops

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 from graphs import *
 from data import *
-# Create tags
-tag1 = Node(tagData[0])
-tag2 = Node(tagData[1])
-tag3 = Node(tagData[2])
-# Test tags
-print(tag1)
-print(tag2)
-print(tag3)
-# Create games
-game1 = Node(gameData[0][0], gameData[0][1], gameData[0][2], gameData[0][3])
-# Test games
-print(game1.name)
-print(game1.tags)
-print(game1.description)
-print(game1.price)
+
 # Create graph
 graph = Graph()
-graph.add_vertex(game1)
-graph.add_vertex(tag1)
-graph.add_vertex(tag2)
-graph.add_vertex(tag3)
+# Create & add games
+for entry in gameData:
+    graph.add_vertex(Node(entry[0], entry[1], entry[2], entry[3]))
+# Create & add tags
+for tag in tagData:
+    graph.add_vertex(Node(tag))
 # Add edges
 graph.add_edges()
-game1.get_edges()
+# game1.get_edges()
 
 print("Browse games by category")
 print("Here are the available categories:")
